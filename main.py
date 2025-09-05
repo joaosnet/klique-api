3c13f15866199e97f93c6859f782f935a4a0059b
João Natividade <87316339+joaosnet@users.noreply.github.com>
2025-09-05 17:29:50 -0300

    feat: Atualizar configuração do cliente Gemini com variáveis de ambiente e melhorar a conexão com o MongoDB

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from gemini_webapi import GeminiClient
 
+from src.config import SECRET_KEY, SECURE_1PSID, SECURE_1PSIDTS
 from src.database import close_mongo_connection, connect_to_mongo
 from src.logger import logger
 from src.routers import auth, batch, generate, register
@@ -15,7 +16,12 @@ from src.services.gemini import GeminiService
 async def lifespan(app: FastAPI):
     # Inicia o cliente Gemini e o armazena no estado da aplicação
     connect_to_mongo()
-    client = GeminiClient()
+    client = GeminiClient(
+        secret_key=SECRET_KEY,
+        secure_1psid=SECURE_1PSID,
+        secure_1psidts=SECURE_1PSIDTS,
+        context=7
+    )
     await client.init()
     app.state.gemini = client
     app.state.gemini_service = GeminiService()
